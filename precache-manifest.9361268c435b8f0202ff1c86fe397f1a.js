8c4b010d345e56ab09bd6c4d0b5eb198d8a8f995
nicola <nicola@users.noreply.github.com>
2020-09-24 21:05:41 +0100

    Updates

---
+++ precache-manifest.9361268c435b8f0202ff1c86fe397f1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516ff1f69186288aff17fae23c1894ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfdb487d501fae4a7a2a",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5099eb262b620b7ff9f1",
+    "url": "./static/css/main.1ece5af4.chunk.css"
+  },
+  {
+    "revision": "cfdb487d501fae4a7a2a",
+    "url": "./static/js/2.77999f5b.chunk.js"
+  },
+  {
+    "revision": "163083841c102495360d4d3ba09a6aac",
+    "url": "./static/js/2.77999f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5099eb262b620b7ff9f1",
+    "url": "./static/js/main.76149850.chunk.js"
+  },
+  {
+    "revision": "5623019795e5d6499f9c",
+    "url": "./static/js/runtime-main.06031c8c.js"
+  }
+]);
