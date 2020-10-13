ab62abe50c1076c47b62b1d458a420d05be05307
nikkolasg <nikkolasg@gmail.com>
2020-10-13 20:32:32 +0100

    cleanup on app

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App () {
   const [miners, setMiners] = useState()
   const [head, setHead] = useState()
   const [node, setNode] = useState('wss://node.glif.io/space07/lotus/rpc/v0')
-  //const [node, setNode] = useState('wss://lotus.jimpick.com/spacerace_api/0/node/rpc/v0')
   const [client, setFilClient] = useState(new Filecoin(node))
   const [spa, setSpa] = useState()
 
@@ -33,13 +32,13 @@ function App () {
     reload()
   }, [node])
 
-/*  useEffect(() => {*/
-    //if (!head) return
+  useEffect(() => {
+    if (!head) return
 
-    //client.fetchPower(head).then(power => {
-      //setSpa(power)
-    //})
-  //}, [head, client])
+    client.fetchPower(head).then(power => {
+      setSpa(power)
+    })
+  }, [head, client])
 
   useEffect(() => {
     let mounted = true
