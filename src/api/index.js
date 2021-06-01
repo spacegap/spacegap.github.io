b1748c7b9d0aa5992caddf42c3fb041e4285283a
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-06-01 19:22:06 +0300

    Update backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export const getHomepage = (onData, onError) => {
-  const socket = new WebSocket('ws://spacegap-backend.herokuapp.com');
+  const socket = new WebSocket('wss://spacegap-backend.herokuapp.com');
   socket.onopen = (event) => { }
 
   socket.onmessage = (event) => {
