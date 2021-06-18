d81ee7c0b32b290256399e620890f38af599c3ee
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-06-18 13:12:00 +0300

    Change api endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export const getHomepage = (onData, onError) => {
-  const socket = new WebSocket('wss://spacegap-backend.herokuapp.com');
+  const socket = new WebSocket('wss://api.spacegap.d.interplanetary.one');
   socket.onopen = (event) => { }
 
   socket.onmessage = (event) => {
